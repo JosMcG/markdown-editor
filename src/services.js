a6f9aa061bff2dc7947e3cd31c81e3dbe43d4e2f
Josilyn McGuinness <josilyn_butler@yahoo.com>
2023-07-04 10:46:52 -0400

    adding firebase files

---
+++ src/services.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcOFB6UksXfUmjZyO4Bml-PyONO4izdy8",
+  authDomain: "markdown-editor-5bec6.firebaseapp.com",
+  projectId: "markdown-editor-5bec6",
+  storageBucket: "markdown-editor-5bec6.appspot.com",
+  messagingSenderId: "33186962938",
+  appId: "1:33186962938:web:0083e782f84a5f90e8d843"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
